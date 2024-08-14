f9c02638a0afca32a59efb365f169acdb32d9a20
ashu <asheesh.pal@tothenew.com>
2024-08-14 12:51:19 +0530

    backend-css

--- js/toolbar.js
+++ js/toolbar.js
@@ -21,6 +21,37 @@
                 console.log('double clicked');
                 $('body').removeClass('dark-theme');
               });
+              function toggleFullscreen(elem) {
+                elem = elem || document.documentElement;
+
+                if (!document.fullscreenElement && !document.mozFullScreenElement &&
+                    !document.webkitFullscreenElement && !document.msFullscreenElement) {
+
+                    if (elem.requestFullscreen) {
+                        elem.requestFullscreen();
+                    } else if (elem.msRequestFullscreen) {
+                        elem.msRequestFullscreen();
+                    } else if (elem.mozRequestFullScreen) {
+                        elem.mozRequestFullScreen();
+                    } else if (elem.webkitRequestFullscreen) {
+                        elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+                    }
+                } else {
+                    if (document.exitFullscreen) {
+                        document.exitFullscreen();
+                    } else if (document.msExitFullscreen) {
+                        document.msExitFullscreen();
+                    } else if (document.mozCancelFullScreen) {
+                        document.mozCancelFullScreen();
+                    } else if (document.webkitExitFullscreen) {
+                        document.webkitExitFullscreen();
+                    }
+                }
+            }
+            // jQuery event handler for the click event
+            $('.button-zoom-maximize').on('click', function() {
+                toggleFullscreen();
+            });
         });
       }
     };
